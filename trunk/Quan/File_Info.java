7535c4e1db7fd76bd6194099ad1aa4e14ab6e620
quan.nguyenvan000@gmail.com <quan.nguyenvan000@gmail.com@2ae334cf-80d2-015b-0e53-173f62bd2b55>
2011-10-12 15:57:54 +0000

    2 Class File_Info.java and ListFile.java kem theo file huong dan ListFile readme.txt

---
+++ trunk/Quan/File_Info.java
@@ -0,0 +1,98 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package listfile;
+
+/**
+ * Class ListFile to generate a list to manager file list
+ * @author KuDo
+ */
+import java.util.ArrayList;
+public class File_Info {
+
+    private String FileName;
+        //set FileName
+        private void setFileName(String name){
+            this.FileName = name;
+        }
+        //get FileName
+        public String getFileName(){
+            return this.FileName;
+        }
+    private String FileHash;
+        private void setFileHash(String hash){
+            this.FileHash = hash;
+        }
+        public String getFileHash(){
+            return this.FileHash;
+        }
+    private int FileSize;// don vi kB
+        //set FileSize
+        private void setFileSize(int size){
+            this.FileSize = size;
+            this.BlockNumber = this.getBlockNumber();
+            this.setupBlock();
+        }
+        //get FileSize
+        public int getFileSize(){
+            return this.FileSize;
+        }
+        //get Number of Block
+        // 1 block = 64kB = 65536B
+        private int getBlockNumber(){
+            return (int) (this.FileSize/64) + 1 ;
+        }
+    private ArrayList <String> IPList ;//= new ArrayList <String>();
+        /* IPList contain IP which client can download from this IP 
+         */
+        //fucntion to addIP to IPList
+        public void addIP(String ip){
+            IPList.add(ip);
+        }
+        //fuction to getIpList
+        public ArrayList getIpList(){
+            return this.IPList;
+        }
+        //function to remove an IP from IPList
+        public void removeIp(String ip){
+            IPList.remove(ip);
+        }
+        //function to get size of IPList
+        public int getIPNum(){
+            return IPList.size();
+        }
+    private Integer BlockNumber;
+        public int getBlockNum(){
+            return this.BlockNumber;
+        }
+    private int[] BlockStatus ;
+    
+    /*
+     * Block status can has 3 value
+     * 0 - if block had never downloaded
+     * 1 - if block is downloading
+     * 2 - if block downloaded.
+     */
+        public void setDownloading(int BlockID){
+            BlockStatus[BlockID] = 1;
+        }
+        public void setDownloaded(int BlockID){
+            BlockStatus[BlockID] = 2;
+        }
+        private void setupBlock(){
+            for(int i = 0 ; i < this.BlockNumber; i ++) BlockStatus[i] = 0;
+        }
+        public int[] getBlockStatus(){
+            return this.BlockStatus;
+        }
+        //constructor
+    File_Info(String name, String size, String hash) {
+        IPList = new ArrayList <String>();
+        BlockStatus = new int[65536];
+        for(int i = 0; i < 65536; i++) BlockStatus[i] = -1;
+        this.setFileName(name);
+        this.setFileSize(Integer.parseInt(size));
+        this.setFileHash(hash);
+    }
+}
---
+++ trunk/Quan/ListFile.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package listfile;
+
+/**
+ *
+ * @author KuDo
+ */
+import java.util.ArrayList;
+public class ListFile {
+    private ArrayList<File_Info> file;
+    ListFile(){
+        file = new ArrayList<File_Info>();
+    }
+    //function to add new file to List
+    public void addFile(String name, String size, String hash){
+        File_Info x = new File_Info(name, size, hash);
+        file.add(x);
+    }
+    //function to remove a File from List
+    //return true if list has that file & remove ok
+    //return false if list doesn't have that file 
+    public boolean removeFile(String hash){
+        int i = this.findFile(hash);
+        if( i == -1 ) return false;
+        file.remove(i);
+        return true;
+    }
+    //find index of a File from hash
+    private int findFile(String hash){
+        for(int i = 0; i < file.size(); i++){
+            File_Info x = file.get(i);
+            if(x.getFileHash().equals(hash)){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public int getFileNum(){
+        return file.size();
+    }
+    public File_Info getFileInfo(String hash){
+        int i = this.findFile(hash);
+        return file.get(i);
+    }
+}
