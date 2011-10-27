5a4a1864be7956f353c3bf6396c1dc4370cfc5da
quan.nguyenvan000@gmail.com <quan.nguyenvan000@gmail.com@2ae334cf-80d2-015b-0e53-173f62bd2b55>
2011-10-27 04:04:51 +0000

    doi port thanh 2222

--- Son/Client/Main.java
+++ Son/Client/Main.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Client;
+
 
 /**
  *
@@ -14,7 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Server server = new Server(5555);
+        Server server = new Server(22222);
         GUI UI = new GUI();
         UI.setVisible(true);
         server.listen(UI);
